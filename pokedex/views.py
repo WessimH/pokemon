3132d8a8f016571508dc032283caa5a58563a163
Joeybadd <jnkunga@hotmail.com>
2025-12-18 23:10:33 +0100

    mise en page pokedex taillwind avec des données mockées

--- pokedex/views.py
+++ pokedex/views.py
@@ -4,4 +4,32 @@ import requests
 # Create your views here.
 
 def index(request):
-    return render(request,'pokedex/index.html')
+    
+    pokemons = [
+        {
+            'id': 1, 'name': 'Bulbizarre', 'type': 'Plante', 
+            'color': 'green', 'desc': 'Il y a une graine sur son dos.'
+        },
+        {
+            'id': 4, 'name': 'Salamèche', 'type': 'Feu', 
+            'color': 'red', 'desc': 'La flamme de sa queue vacille.'
+        },
+        {
+            'id': 7, 'name': 'Carapuce', 'type': 'Eau', 
+            'color': 'blue', 'desc': 'Il se cache dans sa carapace.'
+        },
+        {
+            'id': 25, 'name': 'Pikachu', 'type': 'Électrik', 
+            'color': 'yellow', 'desc': 'Il stocke de l\'électricité.'
+        },
+        {
+            'id': 150, 'name': 'Mewtwo', 'type': 'Psy', 
+            'color': 'purple', 'desc': 'Un Pokémon créé par génétique.'
+        },
+        {
+            'id': 143, 'name': 'Ronflex', 'type': 'Normal', 
+            'color': 'gray', 'desc': 'Il ne fait que dormir et manger.'
+        },
+    ]
+    
+    return render(request,'pokedex/index.html', {'pokemons': pokemons})
